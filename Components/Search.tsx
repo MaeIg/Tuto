4143e3d084899a1db62369381f0fe0b3dabdd823
MaeIg <larochmael@hotmail.fr>
2021-01-27 20:03:05 +0100

    Applying styles on Search

--- Components/Search.tsx
+++ Components/Search.tsx
@@ -2,13 +2,27 @@
 // Components/Search.tsx
 
 import React from 'react';
-import { View, TextInput, Button } from 'react-native';
+import { View, TextInput, Button, StyleSheet } from 'react-native';
 
 export default function Search() {
   return (
-    <View >
-        <TextInput placeholder="Nom de l'Amiibo"/>
+    <View style={styles.globalView} >
+        <TextInput style={styles.textinput} placeholder="Nom de l'Amiibo"/>
         <Button title="Rechercher" onPress={() => {}}/>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+    globalView: {
+        marginTop: 20,
+    },
+    textinput: {
+      marginLeft: 5,
+      marginRight: 5,
+      height: 50,
+      borderColor: '#000000',
+      borderWidth: 1,
+      paddingLeft: 5,
+    },
+  });
