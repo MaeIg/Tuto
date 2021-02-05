a881eddaaf5f36743fe008b023664d8d727f68d6
MaeIg <larochmael@hotmail.fr>
2021-02-05 16:16:34 +0100

    Add activityIndicator when API is called

--- Components/Search.tsx
+++ Components/Search.tsx
@@ -2,22 +2,43 @@
 // Components/Search.tsx
 
 import React, { useState } from 'react';
-import { StyleSheet, View, TextInput, Button, FlatList } from 'react-native';
+import { StyleSheet, View, TextInput, Button, FlatList, ActivityIndicator } from 'react-native';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import AmiiboItem, { AmiiboLayout } from './AmiiboItem';
 import { getAmiiboWithSearchedCharacter } from '../API/AmiiboAPI';
 
 export default function Search() {
   const [amiibos, setAmiibos] = useState<AmiiboLayout[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   let searchText : string = '';
 
   const loadAmiibos = async () : Promise<void> => {
     try {
+      setIsLoading(true);
       const data : { amiibo: AmiiboLayout[] } = await getAmiiboWithSearchedCharacter(searchText);
       setAmiibos(data.amiibo);
+      setIsLoading(false);
     } catch (error) {
       console.error(error);
+    }
+  };
+
+  const displayAmiibos = () : JSX.Element => {
+    if (isLoading) {
+      return (
+        <View style={styles.activityIndicatorContainer}>
+          <ActivityIndicator size="large" color="blue" />
+        </View>
+      );
+    } else {
+      return (
+        <FlatList
+          data={amiibos}
+          keyExtractor={(item) => item.head + item.tail}
+          renderItem={({item}) => <AmiiboItem amiibo={item} />}
+        />
+      );
     }
   };
 
@@ -33,11 +54,7 @@ export default function Search() {
           title="Rechercher"
           onPress={() => { loadAmiibos(); }}
         />
-        <FlatList
-          data={amiibos}
-          keyExtractor={(item) => item.head + item.tail}
-          renderItem={({item}) => <AmiiboItem amiibo={item} />}
-        />
+        { displayAmiibos() }
     </View>
   );
 }
@@ -46,6 +63,11 @@ const styles = StyleSheet.create({
     mainContainer: {
       flex: 1,
         marginTop: 20,
+    },
+    activityIndicatorContainer: {
+      flex: 1,
+      alignContent: 'center',
+      justifyContent: 'center',
     },
     textinput: {
       marginLeft: 5,
